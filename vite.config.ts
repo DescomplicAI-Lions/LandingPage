b73d13158aa499174850a7ddab7e13922d3b4587
AstraSama <felippemart777@gmail.com>
2025-10-07 18:47:30 -0300

    para usar no render

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ export default defineConfig(({ mode }) => {
       server: {
         port: 3000,
         host: '0.0.0.0',
+        allowedHosts: ['landingpage-oa9e.onrender.com'],
       },
       plugins: [react()],
       define: {
