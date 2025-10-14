77d270cf02831931bf472779c228bada342896dd
Wanderlei de Moraes <wanderlei.mora3s@gmail.com>
2025-10-13 23:59:44 -0300

    Página 404 melhorada

--- components/NotFound.tsx
+++ components/NotFound.tsx
@@ -1,21 +1,27 @@
-import React from "react"
-import { Link } from "react-router-dom"
+import React from "react";
+import { Link } from "react-router-dom";
+import Lottie from "lottie-react";
+import errorAnimation from "../src/assets/animations/404 Error.json"; 
 
 const NotFound: React.FC = () => {
-    return(
-        <div className="flex flex-col items-center justify-center h-[70vh] text-center p-6">
-            <h1 className="txext-6x1 font-bold text-primary mb-4">404</h1>
-            <p className= "text-lg text-light-text mb-6">
-            Ops! Esta Página não existe.
-            </p>
-            <Link
-            to="/"
-            className="inline-block bg-primary text-white font-semibold px-8 py-3 rounded-lg shadow-md hover:bg-primary-dark transition"
-            >
-                Voltar para a página inicial
-              </Link>
-            </div>
-    )
-}
+  return (
+    <div className="flex flex-col items-center justify-center h-[70vh] text-center p-6">
+      <div className="w-64 h-64 mb-4">
+        <Lottie animationData={errorAnimation} loop={true} />
+      </div>
 
-export default NotFound
+      <p className="text-lg text-light-text mb-6">
+        Ops! Esta página não existe.
+      </p>
+
+      <Link
+        to="/"
+        className="inline-block bg-primary text-white font-semibold px-8 py-3 rounded-lg shadow-md hover:bg-primary-dark transition"
+      >
+        Voltar para a página inicial
+      </Link>
+    </div>
+  );
+};
+
+export default NotFound;
